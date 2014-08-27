c6556922d307697382b08563f067dc7d0dad3f0a
aspalding <andrewtspalding@gmail.com>
2014-08-27 15:08:39 -0500

    fixes runner.rb

--- runner.rb
+++ runner.rb
@@ -1,7 +1,7 @@
 include Java
+require './assets/SimpleServer.jar'
 require './assets/TicTacServer.jar'
 
-#Java::Application.new.setUp(ARGV.to_java(:String))
-#Java::Application::runLoop
-
-Java::TicTacApp::run(ARGV.to_java(:String))
+Java::TicTacApp::initializeGames
+Java::Application.new.setUp(ARGV.to_java(:String))
+Java::Application::runLoop(Java::TicTacToeRouter.new)
--- src/main/java/TicTacApp.java
+++ src/main/java/TicTacApp.java
@@ -10,16 +10,6 @@ public class TicTacApp {
         }
     }
 
-    public static void run(String args[]){
-        try {
-            initializeGames();
-            new Application().setUp(args);
-            Application.runLoop(new TicTacToeRouter());
-        } catch(Exception e){
-            System.out.println("There was a problem!");
-        }
-    }
-
     public static void initializeGames(){
         Games.games.add(new TicTacToe());
     }
